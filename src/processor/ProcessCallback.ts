16f47444e66b4dcbc5855cfade9d93a16715e4a7
Daniel Steingrube <daniel.steingrube@scalenc.com>
2021-08-13 09:42:04 +0200

    patch: passing this instead of state to allow to inject nc text

--- src/processor/ProcessCallback.ts
+++ src/processor/ProcessCallback.ts
@@ -2,14 +2,20 @@ import { Block, GCode, Instruction, MCode, NcText } from '../models';
 import { State } from './State';
 import { Variables } from './Variables';
 
+export interface Process {
+  state: State;
+  process(ncText: NcText): void;
+  processRelative(ncText: NcText): void;
+}
+
 export interface ProcessCallback {
-  onEnterBlock?(blockIndex: number, block: Block, ncText: NcText, state: State): void;
-  onInstruction?(instruction: Instruction, state: State): void;
-  onUnhandledMCode?(mCode: MCode, state: State): void;
-  onUnhandledGCode?(gCode: GCode, state: State): void;
-  onWait?(waitDelay: number | undefined, state: State): void;
-  onMotion?(start: Variables, end: Variables, state: State): void;
-  onEnterSubprogram?(subProgramName: string, subProgram: NcText, state: State): void;
-  onLeaveSubprogram?(subProgramName: string, subProgram: NcText, state: State): void;
-  onFinish?(state: State): void;
+  onEnterBlock?(blockIndex: number, block: Block, ncText: NcText, process: Process): void;
+  onInstruction?(instruction: Instruction, process: Process): void;
+  onUnhandledMCode?(mCode: MCode, process: Process): void;
+  onUnhandledGCode?(gCode: GCode, process: Process): void;
+  onWait?(waitDelay: number | undefined, process: Process): void;
+  onMotion?(start: Variables, end: Variables, process: Process): void;
+  onEnterSubprogram?(subProgramName: string, subProgram: NcText, process: Process): void;
+  onLeaveSubprogram?(subProgramName: string, subProgram: NcText, process: Process): void;
+  onFinish?(process: Process): void;
 }
